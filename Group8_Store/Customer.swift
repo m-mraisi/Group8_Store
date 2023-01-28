901aec6999291207ca8946c67c9658a14e72bd73
m-mraisi <mahmoudmraisi2@gmail.com>
2023-01-28 16:17:38 -0500

    Revert "fixed the git issue"
    This reverts commit 71b3bfa74b4a7d95f2dacaed244b127d2b8ebdd0.

--- Group8_Store/Customer.swift
+++ Group8_Store/Customer.swift
@@ -1,28 +1,19 @@
-//
-//  Customer.swift
-//  Group8_Store
-
 import Foundation
+
 class Customer {
-    var balance: Double = 10.0
+    var balance: Double = 0.0
     var itemsList: [OwnedItem] = []
     
-    init(balance: Double, itemsList: [OwnedItem]) {
-        self.balance = balance
-        self.itemsList = itemsList
+    func reloadAmount( _amt: Double) {
+        self.balance = self.balance + _amt
     }
     
-    func reloadAmount( _ amount: Double) {
-        self.balance = self.balance + amount
-    }
-    
-    func useItem (_ id:String, _ minutes:Int) {
+    func useItem (_id:String, _minutes:Int) {
         //find id where it matches
         for item in self.itemsList {
-            if(item.id == id) {
-                item.minutesUsed = item.minutesUsed + minutes
+            if(item.id == _id) {
+                item.minutesUsed = item.minutesUsed + _minutes
             }
         }
     }
 }
-
